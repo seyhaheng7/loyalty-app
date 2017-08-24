d41bb6ab9eafbd5643260faa144063853118bdbb
seyhaheng <khdaap@gmail.com>
2017-08-24 09:05:42 +0700

    setup Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError do
+    redirect_to root_url, alert: "You can't access to this page"
+  end
   protect_from_forgery with: :exception
 end
