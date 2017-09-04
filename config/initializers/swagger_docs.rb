8a457d715cf1f5200be6dec05581f2dfa6dfb24f
Uysim <uysimty@gmail.com>
2017-09-04 11:25:19 +0700

    swagger docs base domain

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -8,7 +8,7 @@ end
 Swagger::Docs::Config.register_apis({
   "1.0" => {:controller_base_path => "",
             :api_file_path => "public/apidocs",
-            :base_path => "http://localhost:3000",
+            :base_path => "http://#{ENV['BASE_DOMAIN']}",
             :clean_directory => true
           }
 })
