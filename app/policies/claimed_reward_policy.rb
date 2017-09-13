985122ee77ed2faa88d298e245835458ce233812
Vandy Sodanheang <vandysodanheang@gmail.com>
2017-09-13 08:13:06 +0000

    Feature-Admin-Receiptionist-Approver

--- app/policies/claimed_reward_policy.rb
+++ app/policies/claimed_reward_policy.rb
@@ -1,6 +1,15 @@
 class ClaimedRewardPolicy < ApplicationPolicy
+
+  def index?
+    user.approver? || user.receptionist? || user.admin? || user.super?
+  end
+
+  def show?
+    user.approver? || user.receptionist? || user.admin? || user.super?
+  end
+
   def approve?
-    record.submitted? and ( user.admin? || user.super? )
+    record.submitted? and ( user.approver? || user.admin? || user.super? )
   end
 
   def reject?
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,30 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.super?
+  end
+
+  def show?
+    user.admin? || user.super?
+  end
+
+  def create?
+    user.admin? || user.super?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.admin? || user.super?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.admin? || user.super?
+  end
+
 end
