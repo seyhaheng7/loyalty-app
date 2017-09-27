6a6411062a6ca8bb4f743f00dc86045a940ad39f
Kanitha <chimkanitha@gmail.com>
2017-09-27 13:07:52 +0700

    action cable

--- app/jobs/notification_relay_job.rb
+++ app/jobs/notification_relay_job.rb
@@ -2,7 +2,7 @@ class NotificationRelayJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # html = ApplicationController.render partial: "notifications/#{notification.objectable_type.underscore.pluralize}/#{notification.action}", locals: {notification: notification}, formats: [:html]
+    html = ApplicationController.render partial: "notifications/top_nav", locals: {notification: notification}, formats: [:html]
     ActionCable.server.broadcast "notifications:#{notification.notifyable_id}", html: html
   end
 end
