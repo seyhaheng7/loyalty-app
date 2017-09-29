5afbd76b273a1bf70c568d810428be81534ad8f2
Jane Ly <chimkanitha@gmail.com>
2017-09-29 09:11:21 +0000

    Feature all notification api

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :notification do
-    notification_type "MyString"
+    notification_type "SubmittedReceipt"
     text "MyString"
+    association :notifyable, factory: :user
+    association :objectable, factory: :receipt 
   end
 end
