841d58a2b7cdfda7731bac1fdb11a364893fdd89
seyhaheng <khdaap@gmail.com>
2017-10-04 08:46:52 +0000

    Improve receipt filter

--- app/grids/receipts_grid.rb
+++ app/grids/receipts_grid.rb
@@ -7,6 +7,15 @@ class ReceiptsGrid
 
   filter(:receipt_id, :string)
   filter(:status, :enum, select: Receipt.aasm.states.map(&:name))
+  filter(:store_id, :enum,
+   :select => lambda {Store.pluck(:name, :id)})
+  filter(:customer_id, :enum,
+   :select => lambda {Customer.all.map {|p| [p.name, p.id]}})
+
+  filter(:total, :integer, range: true)
+  filter(:earn_points, :integer, range: true)
+  filter(:managed_by_id, :enum, :select => lambda {User.all.map {|p| [p.name, p.id]}})
+
 
   column(:receipt_id)
   column(:status)
