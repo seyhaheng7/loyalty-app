bc76e773b72354e2b9fb5dd14efd606dda830e62
seyhaheng <khdaap@gmail.com>
2017-10-05 10:16:06 +0700

    (improve): rewards filter

--- app/grids/rewards_grid.rb
+++ app/grids/rewards_grid.rb
@@ -5,12 +5,12 @@ class RewardsGrid
     Reward
   end
 
-	filter(:name, :string)
-	filter(:store_id, :enum, :select => lambda {Store.pluck(:name, :id)})
-	filter(:quantity, :integer, range: true)
+  filter(:name, :string)
+  filter(:store_id, :enum, :select => lambda {Store.pluck(:name, :id)})
+  filter(:quantity, :integer, range: true)
   filter(:require_points, :integer, range: true)
-	filter(:price, :integer, rangenge: true)
-	filter(:reward_id, :enum, select: ["available", "unvailable"])
+  filter(:price, :integer, rangenge: true)
+  filter(:reward_id, :enum, select: ["available", "unvailable"])
 
   column(:name)
   column(:image, html: true) do |reward|
