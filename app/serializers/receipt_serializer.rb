1f2b69c04fde7439c3380fa670c77af403e3cec5
Uysim <uysimty@gmail.com>
2017-10-10 16:21:57 +0700

    (improve): created at in receipt json

--- app/serializers/receipt_serializer.rb
+++ app/serializers/receipt_serializer.rb
@@ -1,5 +1,5 @@
 class ReceiptSerializer < ActiveModel::Serializer
-  attributes :id, :receipt_id, :total, :capture, :earned_points, :status, :errors
+  attributes :id, :receipt_id, :total, :capture, :earned_points, :status, :errors, :created_at
   attributes :errors
   belongs_to :store
   belongs_to :customer
