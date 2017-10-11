fd6d6da16766735bc54844469364aedaab58cce3
Uysim <uysimty@gmail.com>
2017-10-11 16:10:07 +0700

    (api): add distance to store

--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,6 +1,12 @@
 class StoreSerializer < ActiveModel::Serializer
-  attributes :id, :name, :lat, :long, :address
+  attributes :id, :name, :lat, :long, :address, :distance
   attributes :errors
   belongs_to :company
   belongs_to :location
+
+  def distance
+    if object.respond_to?(:distance)
+      object.distance
+    end
+  end
 end
