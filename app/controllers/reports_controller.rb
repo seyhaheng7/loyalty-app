c629b6aafd6222621ab1343109a20304e56e6c6f
seyhaheng <khdaap@gmail.com>
2017-10-11 11:25:36 +0700

    (improve): report reward page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,8 @@
 class ReportsController < ApplicationController
   def rewards
+    authorize :report
     @grid = ReportRewardGrid.new(params[:report_reward_grid]) do |scope|
       scope.page(params[:page])
     end
-    authorize @grid.assets
   end
 end
