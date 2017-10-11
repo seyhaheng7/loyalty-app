19a0ee734b6ae5ccfb40a1bcc760c767480ede2d
seyhaheng <khdaap@gmail.com>
2017-10-11 03:29:56 +0000

    (feature): limit log file 40m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 40 * 1024 * 1024)
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
