f1a2fb5ece64a9d414d413ced04d7609893ff86b
Ty Uysim <uysimty@gmail.com>
2017-10-17 03:17:48 +0000

    (fix): assets sync production

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,15 +1,14 @@
-if ENV['ENABLE_S3'] == 'true'
-  AssetSync.configure do |config|
-    config.fog_provider = 'AWS'
-    config.fog_directory = ENV['FOG_DIRECTORY']
-    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+AssetSync.configure do |config|
+  config.enabled = (ENV['ENABLE_S3'] == 'true')
+  config.fog_provider = 'AWS'
+  config.fog_directory = ENV['FOG_DIRECTORY']
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
 
 
-    config.existing_remote_files = 'delete'
+  config.existing_remote_files = 'delete'
 
-    config.fog_region = ENV['FOG_REGION']
-    config.gzip_compression = true
-    config.custom_headers = { '.*' => { cache_control: 'max-age=315576000', expires: 1.year.from_now.httpdate } }
-  end
+  config.fog_region = ENV['FOG_REGION']
+  config.gzip_compression = true
+  config.custom_headers = { '.*' => { cache_control: 'max-age=315576000', expires: 1.year.from_now.httpdate } }
 end
