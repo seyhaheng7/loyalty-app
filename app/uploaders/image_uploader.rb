3a0f4d5a8d99e029c978c965e9d85e2a6299bc69
seyhaheng <khdaap@gmail.com>
2017-10-24 16:19:12 +0700

    (feature): upload file with time

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,4 +10,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     'default.png'
   end
 
+  def filename
+    @name ||= "#{timestamp}-#{super}" if original_filename.present? and super.present?
+  end
+
+  def timestamp
+    var = :"@#{mounted_as}_timestamp"
+    model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.to_i)
+  end
+
 end
