83f9b65b927d0353f2f415fee35aa51e583ecaeb
Uysim <uysimty@gmail.com>
2017-10-25 09:57:19 +0700

    (fix): approved reject reward notification

--- app/workers/rejected_claimed_reward_notifications_worker.rb
+++ app/workers/rejected_claimed_reward_notifications_worker.rb
@@ -5,7 +5,7 @@ class RejectedClaimedRewardNotificationsWorker
   def perform(claimed_reward_id)
     claimed_reward = ClaimedReward.find claimed_reward_id
     customer = claimed_reward.customer
-    notification = receipt.notifications.new
+    notification = claimed_reward.notifications.new
     notification.notifyable = customer
     notification.notification_type = "RejectedClaimedReward"
     notification.text = "Your reward claim was rejected!"
