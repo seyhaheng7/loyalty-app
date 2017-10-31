e2708938ef71f2f34dff152f0660455681dd93b6
Uysim <uysimty@gmail.com>
2017-10-31 08:50:46 +0700

    (improve): add youtube id to guide

--- app/serializers/guide_serializer.rb
+++ app/serializers/guide_serializer.rb
@@ -1,4 +1,4 @@
 class GuideSerializer < ActiveModel::Serializer
-  attributes :id, :title, :youtube_url, :embed_url, :thumbnail
+  attributes :id, :title, :youtube_url, :embed_url, :thumbnail, :youtube_id
   attributes :errors
 end
