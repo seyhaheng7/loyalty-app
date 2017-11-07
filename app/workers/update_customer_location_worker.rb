07b44cef83dbf443026402b7db8696c812c70f4a
Uysim <uysimty@gmail.com>
2017-11-07 10:35:47 +0700

    (fix): user location history

--- app/workers/update_customer_location_worker.rb
+++ app/workers/update_customer_location_worker.rb
@@ -4,5 +4,26 @@ class UpdateCustomerLocationWorker
   def perform(customer_id)
     customer = Customer.find customer_id
     ActionCable.server.broadcast "customer_location_channel", customer: customer
+    add_customer_location_history(customer)
+  end
+
+
+  def add_customer_location_history(customer)
+    lat = customer.lat
+    long = customer.long
+    customer_id = customer.id
+    land_marks = Geocoder.search("#{lat},#{long}")
+    if land_marks.present?
+      land_mark = land_marks.select{ |lm| lm.include?('neighborhood') }.first
+      if land_mark.blank?
+        land_mark = land_marks.first
+      end
+      land_mark_name = land_mark.formatted_address
+      land_mark = LandMark.first_or_create(name: land_mark_name)
+      customer_land_marks = CustomerLandMark.where(customer_id: customer_id, land_mark: land_mark).today
+      if customer_land_marks.blank?
+        CustomerLandMark.create(customer_id: customer_id, lat: lat, long: long, land_mark: land_mark)
+      end
+    end
   end
 end
