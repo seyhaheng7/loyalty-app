deb573aa0ea88df9d17bebee4dc19225ae789844
Uysim <uysimty@gmail.com>
2017-11-07 13:57:23 +0700

    (feature): notify merchant when reward approved

--- app/workers/approved_claimed_reward_notifications_worker.rb
+++ app/workers/approved_claimed_reward_notifications_worker.rb
@@ -4,12 +4,27 @@ class ApprovedClaimedRewardNotificationsWorker
 
   def perform(claimed_reward_id)
     claimed_reward = ClaimedReward.find claimed_reward_id
+
+  end
+
+  def notify_customer(claimed_reward)
     customer = claimed_reward.customer
     notification = claimed_reward.notifications.new
     notification.notifyable = customer
     notification.notification_type = "ApprovedClaimedReward"
     notification.text = "Your reward claim was approved!"
     notification.save
+  end
+
+  def notify_merchant(claimed_reward)
+    store = claimed_reward.store
+    store.merchants.each do |merchant|
+      notification = claimed_reward.notifications.new
+      notification.notifyable = merchant
+      notification.notification_type = "ApprovedClaimedReward"
+      notification.text = "Reward claimed was approved!"
+      notification.save
+    end
 
   end
 end
