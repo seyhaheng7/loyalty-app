b0a59f3cdf674514e76f334c90c7289e6503f988
Uysim <uysimty@gmail.com>
2017-11-13 08:52:44 +0700

    add create to chat data

--- app/serializers/chat_datum_serializer.rb
+++ app/serializers/chat_datum_serializer.rb
@@ -1,6 +1,6 @@
 class ChatDatumSerializer < ActiveModel::Serializer
-  attributes :id, :text, :sticker, :audio, :data_type
+  attributes :id, :text, :sticker, :audio, :data_type, :created_at
   attributes :errors
-  
+
   belongs_to :customer
 end
