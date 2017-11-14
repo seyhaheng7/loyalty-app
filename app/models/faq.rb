d0b1cfeb45690cdb270d97e06c704ef7f2403a5e
seyhaheng7 <khdaap@gmail.com>
2017-11-14 15:24:26 +0700

    (fixing): FAQ

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -3,4 +3,7 @@ class Faq < ApplicationRecord
   validates :title, presence: true
 
   default_scope { order(created_at: :desc) }
+
+  scope :title_like, ->(title){ where("#{table_name}.title ilike ?", "%#{title}%") }
+
 end
