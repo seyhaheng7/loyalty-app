d4707f1cf7e167475500e1ffdb196d1e7ca0bf64
seyhaheng <khdaap@gmail.com>
2017-11-15 07:38:33 +0000

    (fixing): advertisement

--- app/grids/advertisements_grid.rb
+++ app/grids/advertisements_grid.rb
@@ -6,18 +6,17 @@ class AdvertisementsGrid
     Advertisement
   end
 
-  filter(:name, :string)
+  filter(:name, :string){ |value, scope| scope.name_like(value) }
   filter(:phone, :string)
-  filter(:address, :string)
+  filter(:address, :string){ |value, scope| scope.address_like(value) }
 
   column(:name)
-  column(:banner, html: true) do |record|
+  column(:banner, html: true, order: false) do |record|
     image_tag record.banner, size: '50x50'
   end
   column(:for_page, header: 'Page')
   column(:active)
-  column(:address)
-  column(:phone)
+  column(:phone, order: false)
 
   # column(:created_at) do |model|
   #   model.created_at.to_date
--- app/models/store_banner.rb
+++ app/models/store_banner.rb
@@ -3,6 +3,6 @@ class StoreBanner < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :store
 
-  validates :image, presence: true, file_geometry: {is: [700, 300]}
+  validates :image, presence: true, file_geometry: {is: [700, 300]}, if: :image_changed?
 
 end
