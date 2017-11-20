394e6eb946d44c092bafb409b5dd2b4315114e15
Uysim <uysimty@gmail.com>
2017-11-20 15:43:15 +0700

    fix assets domain

--- config/application.rb
+++ config/application.rb
@@ -36,12 +36,7 @@ module CodingateProject
     config.time_zone = 'Bangkok'
 
     if ENV['ENABLE_S3'] == 'true'
-      if ENV['ASSETS_HOST'].present?
-        config.action_controller.asset_host = ENV['ASSETS_HOST']
-      else
-        config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-      end
-
+      config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
       config.assets.prefix = "/assets"
     else
       config.action_controller.asset_host = ENV['ASSETS_HOST']
