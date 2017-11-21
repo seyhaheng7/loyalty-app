d44c4ae653de788f1f38240f2ddc90b4beac3fe1
Uysim <uysimty@gmail.com>
2017-11-21 11:52:47 +0700

    fix production fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
   config.asset_host = ActionController::Base.asset_host
   if ENV['ENABLE_S3'] == 'true'
     if Rails.env.staging? || Rails.env.production?
-      config.storage = :fog
       config.fog_provider = 'fog/aws'
       config.fog_credentials = {
         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
@@ -11,6 +10,7 @@ CarrierWave.configure do |config|
         provider: 'AWS',
         region: ENV['FOG_REGION']
       }
+      config.storage = :fog
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
       config.fog_directory = ENV['FOG_DIRECTORY']
     end
