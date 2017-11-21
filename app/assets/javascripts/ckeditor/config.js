66eafe03f51ef20d61dda0a1562c5f12c7a562db
Uysim <uysimty@gmail.com>
2017-11-21 10:03:23 +0700

    limit ckeditor

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,12 @@
+CKEDITOR.editorConfig = function( config )
+{
+
+  config.toolbar = [
+    ["Bold",  "Italic",  "Underline",  "-",  'Link', 'Unlink', '-', 'NumberedList', 'BulletedList']
+  ];
+
+  config.toolbar_mini = [
+    ["Bold",  "Italic",  "Underline",  "-",'Link', 'Unlink', '-', 'NumberedList', 'BulletedList'],
+  ];
+  config.toolbar = "mini";
+};
