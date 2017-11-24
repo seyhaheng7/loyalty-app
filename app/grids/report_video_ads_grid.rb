bcfa8f7a032a2a5b6992f834c8f55ef221e83e05
seyhaheng7 <khdaap@gmail.com>
2017-11-24 16:31:19 +0700

    (fixing): watch and earn bug from QA

--- app/grids/report_video_ads_grid.rb
+++ app/grids/report_video_ads_grid.rb
@@ -5,23 +5,22 @@ class ReportVideoAdsGrid
     VideoAd
   end
 
-  filter(:title, :string)
-
+  filter(:title, :string, header: 'Company'){ |value, scope| scope.title_like(value) }
   filter(:date, :date, range: true) do |value, scope|
     start_date = value.first
     end_date  = value.second
     scope.active_between(start_date, end_date)
   end
 
-  column(:title) do |record|
+  column(:title, header: 'Company') do |record|
     record.title
   end
   column(:start_date)
   column(:end_date)
-  column(:point) do |record|
+  column(:earned_points, header: 'Point') do |record|
     record.earned_points
   end
-  column(:rich_view) do |record|
+  column(:view_video_ads, header: 'Rich view') do |record|
     record.view_video_ads.sum(:view_count)
   end
 
