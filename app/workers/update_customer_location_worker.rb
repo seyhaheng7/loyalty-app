8068f3f886fd5a1bbec8fab7a23781cb171cbc53
Uysim <uysimty@gmail.com>
2017-11-25 11:51:15 +0700

    fix update location land mark customer

--- app/workers/update_customer_location_worker.rb
+++ app/workers/update_customer_location_worker.rb
@@ -16,7 +16,7 @@ class UpdateCustomerLocationWorker
     if land_marks.present?
       land_mark = land_marks.first
       land_mark_name = land_mark.formatted_address
-      land_mark = LandMark.first_or_create(name: land_mark_name)
+      land_mark = LandMark.where(name: land_mark_name).first_or_create
       customer_land_marks = CustomerLandMark.where(customer_id: customer_id, land_mark: land_mark).today
       if customer_land_marks.blank?
         CustomerLandMark.create(customer_id: customer_id, lat: lat, long: long, land_mark: land_mark)
